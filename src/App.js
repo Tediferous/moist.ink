a7e06b6c25f1b30d651f53536f503a90c1c7f2c0
Ted Dorfeuille <tdorfeuille.zteam@zuora.com>
2019-12-25 15:17:38 -0500

    feat(about): exists

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
-//import {
-//  BrowserRouter as Router,
-//  Switch,
-//  Route,
-//  Link
-//} from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './css/App.scss';
 import './css/animation.scss';
 import './css/vars.scss';
 import Navbar from './navbar'
+import About from './about'
 import Jumbo from './jumbotron'
 import Gallery from './gallery'
 import Bio from './bio'
@@ -20,6 +21,7 @@ function App() {
     <div id="myApp" className="App">
       <Navbar/>
       <Jumbo/>
+      <About/>
       <Gallery/>
       <Bio/>
       <Store/>
